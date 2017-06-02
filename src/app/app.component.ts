6ef58d2027f32231f82f0b8a7defcf27a6199e05
rjcorflo <rj.corchero@gmail.com>
2017-06-02 20:38:39 +0200

    [DEV] Add first Hero with class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,25 @@
 import {Component} from '@angular/core';
 import 'rxjs/add/observable/from';
 
+export class Hero {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'app-root',
   // templateUrl: './app.component.html',
   template: `
     <h1>{{title}}</h1>
-    <h2>{{hero}} details!</h2>
+    <h2>{{hero.name}} details!</h2>
   `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Tour of Heroes';
+  title = 'Tour of Heroes (The Witcher Edition)';
 
-  hero = 'Windstorm';
+  hero: Hero = {
+    id: 1,
+    name: 'Geralt of Rivia'
+  };
 }
