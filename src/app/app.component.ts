f843de6e3f3294b8abf21d847849b34a49254f7b
rjcorflo <rj.corchero@gmail.com>
2017-06-17 00:17:08 +0200

    [DEV] Add hero data service provider

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,6 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Hero} from './hero';
-
-const HEROES: Hero[] = [
-  {id: 1, name: 'Geralt of Rivia'},
-  {id: 2, name: 'Yennefer of Vengerberg'},
-  {id: 3, name: 'Ciri'},
-  {id: 4, name: 'Zoltan Chivay'},
-  {id: 5, name: 'Jaskier'},
-  {id: 6, name: 'Milva'},
-  {id: 7, name: 'Triss Merigold'},
-  {id: 8, name: 'Regis'},
-  {id: 9, name: 'Vesemir'},
-  {id: 10, name: 'Emhyr van Emreis'}
-];
+import {HeroService} from './hero.service';
 
 @Component({
   selector: 'app-root',
@@ -33,12 +21,14 @@ const HEROES: Hero[] = [
       background-color: #CFD8DC !important;
       color: white;
     }
+
     .heroes {
       margin: 0 0 2em 0;
       list-style-type: none;
       padding: 0;
       width: 15em;
     }
+
     .heroes li {
       cursor: pointer;
       position: relative;
@@ -49,19 +39,23 @@ const HEROES: Hero[] = [
       height: 1.6em;
       border-radius: 4px;
     }
+
     .heroes li.selected:hover {
       background-color: #BBD8DC !important;
       color: white;
     }
+
     .heroes li:hover {
       color: #607D8B;
       background-color: #DDD;
       left: .1em;
     }
+
     .heroes .text {
       position: relative;
       top: -3px;
     }
+
     .heroes .badge {
       display: inline-block;
       font-size: small;
@@ -76,14 +70,26 @@ const HEROES: Hero[] = [
       margin-right: .8em;
       border-radius: 4px 0 0 4px;
     }
-  `]
+  `],
+  providers: [HeroService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Tour of Heroes (The Witcher Edition)';
 
-  heroes = HEROES;
+  heroes: Hero[];
 
   selectedHero: Hero;
+
+  constructor(private heroService: HeroService) {
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+  {id: 1, name: 'Geralt of Rivia'},
+  {id: 2, name: 'Yennefer of Vengerberg'},
+  {id: 3, name: 'Ciri'},
+  {id: 4, name: 'Zoltan Chivay'},
+  {id: 5, name: 'Jaskier'},
+  {id: 6, name: 'Milva'},
+  {id: 7, name: 'Triss Merigold'},
+  {id: 8, name: 'Regis'},
+  {id: 9, name: 'Vesemir'},
+  {id: 10, name: 'Emhyr van Emreis'}
+];
