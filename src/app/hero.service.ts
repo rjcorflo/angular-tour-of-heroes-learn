c1874177a3eb504cd26982e38c1561acc0598257
rjcorflo <rj.corchero@gmail.com>
2017-06-17 00:27:04 +0200

    [DEV] Add hero data service provider

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -8,7 +8,14 @@ export class HeroService {
   constructor() {
   }
 
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes(): Promise<Hero[]> {
+    return Promise.resolve(HEROES);
+  }
+
+  getHeroesSlowly(): Promise<Hero[]> {
+    return new Promise(resolve => {
+      // Simulate server latency with 2 second delay
+      setTimeout(() => resolve(this.getHeroes()), 2000);
+    });
   }
 }
